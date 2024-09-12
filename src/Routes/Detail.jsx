61b71ad559eebd34ef4c4ce6daa2bc3512cec8db
Rocio Tellez Lopez <rociotellezlopez@gmail.com>
2024-09-12 10:22:05 -0500

    Modifica componentes y vista

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Detail = () => {
- 
   // Consumiendo el parametro dinamico de la URL deberan hacer un fetch a un user en especifico
+  const [detail, setDetail] = useState({})
+  const { id } = useParams();
+  const url = "https://jsonplaceholder.typicode.com/users/" + id;
+  // console.log(id);
+
+  useEffect(() => {
+    axios(url).then((res) => {
+      console.log(res.data);
+      setDetail(res.data)
+    });
+  }, []);
 
   return (
     <>
       <h1>Detail Dentist id </h1>
       {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
       {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
-    </>
-  )
-}
 
-export default Detail
+      <div>
+        <img src="../../public/images/doctor.jpg" alt="imgDentist" />
+        <h3>{detail.name}</h3>
+        <p>Email: {detail.email}</p>
+        <p>Telefono: {detail.phone}</p>
+        <p>Sitio web: {detail.website}</p>
+      </div>
+    </>
+  );
+};
+
+export default Detail;
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import Card from '../Components/Card'
-
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Card from "../Components/Card";
+
 const Home = () => {
+  const [list, setList] = useState([]);
+  const url = "https://jsonplaceholder.typicode.com/users";
+
+  useEffect(() => {
+    axios(url).then((res) => {
+      console.log(res.data)
+      setList(res.data)
+    });
+  }, []);
+  
   return (
-    <main className="" >
+    <main className="">
       <h1>Home</h1>
-      <div className='card-grid'>
+      <div className="card-grid">
         {/* Aqui deberias renderizar las cards */}
+        {list.map((dentist) => <Card key={dentist.id} {...dentist}/>)}
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
